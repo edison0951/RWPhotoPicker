d1a9a76271a64d8b1ffbe1d9c7520def41521663
王河云 <wangheyun@wesai.com>
2017-01-17 16:37:27 +0800

    add photo

---
+++ RWPhotoPicker/Layout/RWPhotoListLayout.h
@@ -0,0 +1,12 @@
+//
+//  RWPhotoListLayout.h
+//
+//  Created by riven wang on 2017/1/17.
+//  Copyright © 2017年 wesai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWPhotoListLayout : UICollectionViewLayout
+
+@end
---
+++ RWPhotoPicker/Layout/RWPhotoListLayout.m
@@ -0,0 +1,13 @@
+//
+//  RWPhotoListLayout.m
+//  PhotoPickerSample
+//
+//  Created by 王河云 on 2017/1/17.
+//  Copyright © 2017年 wesai. All rights reserved.
+//
+
+#import "RWPhotoListLayout.h"
+
+@implementation RWPhotoListLayout
+
+@end
